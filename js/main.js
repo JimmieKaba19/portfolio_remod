06c4a939077634fa7129f4daff27d6ea0ffb4902
JimmieKaba19 <wambugujimmie994@gmail.com>
2024-08-03 01:59:41 +0300

    feat: skills section development experience and education section start

--- js/main.js
+++ js/main.js
@@ -92,11 +92,32 @@
 
 
     // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+    $(function() {
+
+        $(".progress").each(function() {
+      
+          var value = $(this).attr('data-value');
+          var left = $(this).find('.progress-left .progress-bar');
+          var right = $(this).find('.progress-right .progress-bar');
+      
+          if (value > 0) {
+            if (value <= 50) {
+              right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+            } else {
+              right.css('transform', 'rotate(180deg)')
+              left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+            }
+          }
+      
+        })
+      
+        function percentageToDegrees(percentage) {
+      
+          return percentage / 100 * 360
+      
+        }
+      
+      });
 
 
     // Portfolio isotope and filter
